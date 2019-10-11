e20cf2da2bb05de3d757c9ce7af5d7f939a20bcf
ElmarDusaev <32952623+ElmarDusaev@users.noreply.github.com>
2019-10-11 09:17:45 +0300

    Add Task185

---
+++ Task185/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 2, 13, 3, 34, 5 };
+            int counter = 0;
+            int max = array.Max();
+
+            for (int i = 1; i < array.Length-1; i++)
+            {
+                if (Math.Pow(array[i - 1], 2) + Math.Pow(array[i + 1], 2) == array[i] && array[i]!=max) counter++;
+            }
+
+            Console.WriteLine("Количество чисел:");
+            Console.WriteLine(counter);
+
+            Console.Read();
+        }
+    }
+}
